97bf20ea20ca144fe267cd6ed74c5b598d42f4cf
radudobrila <64649159+radudobrila@users.noreply.github.com>
2023-08-21 00:07:50 +0200

    Aggiunto controllo per -n e -q

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
-// Prova a caso
+// Progetto di Sistemi Operativi
 
 int main(int argc, const char *argv[]){
-    char a = "Yasso\n";
+    int n, q;
+    if(argc < 1){
+        perror("Error: Invalid_Argument\n");
+        return -1;
+    }
+
+    for( int i = 0; i < 4; i++){
+        if(strcmp(argv[i], "-n") == 1) n = argv[i+1];
+        if(strcmp(argv[i], "-q") == 1) q = argv[i+1];
+    }
+
+    
 }
